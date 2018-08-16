eb0f1f79893e7bb3c8d08b01aadf140b4daed75c
Malashykhina <no_parsley@i.ua>
2018-08-16 15:39:05 +0300

    done task 1 2
    Given I'm in the app
    When I navigate to Home page
    Then I see all teams (with team name)
    
    Given I'm on Home page
    When I choose a team
    Then I see Team Details (with team name and people belonging to the team)

---
+++ WebApplication1/Controllers/TeamsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.Core;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TeamsController : Controller
+    {
+        DataService dataService { get; set; }
+        public TeamsController(DataService ds) {
+            dataService = ds;
+        }
+        public IActionResult Index() {
+            var model = dataService.Teams;
+            return View(model);
+        }
+
+        public IActionResult People() {
+            
+            var model = dataService.People;
+            return View(model);
+        }
+
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult TeamDetails(string team) {
+            var model = dataService.getPeopleInTeam(team);
+            return View("~/Views/Teams/People.cshtml", model);
+            //var ds = new DataService();
+            //ds.People= dataService.getPeopleInTeam(team);
+            //ds.Teams = new List<Team> { new Team() { Name=team} };
+            //return View(ds);
+        }
+
+        public IActionResult AddPerson(string team, Person pers) {
+            var model = dataService.getPeopleInTeam(team);
+            dataService.addPersonToTeam(team, pers);
+            
+            return View("~/Views/Teams/People.cshtml", model);
+            //var ds = new DataService();
+            //ds.People= dataService.getPeopleInTeam(team);
+            //ds.Teams = new List<Team> { new Team() { Name=team} };
+            //return View(ds);
+        }
+    }
+}
---
+++ WebApplication1/Models/DataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Catalog.Core
+{
+    public class DataService
+    {
+        public DataService()
+        {
+            var eagles = new Team { Name = "Eagles" };
+            var bears = new Team { Name = "Bears" };
+
+            Teams = new List<Team>
+            {
+                eagles,
+                bears
+            };
+
+            People = new List<Person>
+            {
+                new Person { Name = "John" },
+                new Person { Name = "Joe" },
+                new Person { Name = "Ed", Team = eagles },
+                new Person { Name = "Merphy", Team = bears },
+                new Person { Name = "Met", Team = bears },
+            };
+        }
+
+        public IEnumerable<Team> Teams { get; set; }
+        public IEnumerable<Person> People { get; set; }
+
+        public IEnumerable<Person> getPeopleInTeam (string team){
+            var peopleTeam = People.Where
+                (item => (item.Team != null) && item.Team.Name.Equals(team)).ToList();
+            return peopleTeam;
+        }
+
+        public void addPersonToTeam(string team, Person person) {
+            foreach (var pers in People) {
+                if (pers.Equals(person))
+                    pers.Team.Name = team;
+            }
+        }
+    }
+}
